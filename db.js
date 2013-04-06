f6a0249b6a7b8a147a039f676d517059ff467dee
Ondrej Sika <sika.ondrej@gmail.com>
2013-04-06 22:38:02 +0200

    add user/passwd

--- db.js
+++ db.js
@@ -2,5 +2,5 @@ var mongo = require("mongojs");
 
 collections = ["users", "commons", "tasks"];
 //url = "mongodb://"+user+":"+password+"@"+host+":"+port+"/"+database;
-url = "mongodb://localhost:27017/test";
+url = "mongodb://pes:kocka@10.10.10.100:27017/test";
 exports.db = new mongo.connect(url, collections);
