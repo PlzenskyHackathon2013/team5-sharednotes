84e06ee2d4949de92ec0fd54008a594973a87232
Ondrej Sika <sika.ondrej@gmail.com>
2013-04-06 23:28:41 +0200

    update db conn

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 var mongo = require("mongojs");
 
+user = "admin";
+password = "pwvRIzrSLX5C";
+host = "127.0.0.1";
+port = 27018;
+database = "sharednotes";
+
 collections = ["users", "commons", "tasks"];
-//url = "mongodb://"+user+":"+password+"@"+host+":"+port+"/"+database;
-url = "mongodb://pes:kocka@10.10.10.100:27017/test";
+url = "mongodb://"+user+":"+password+"@"+host+":"+port+"/"+database;
 exports.db = new mongo.connect(url, collections);
