b66ad62b85f1d3c4072bd2dee1e6559341e0f3ff
Ing. Petr BENEDIKT <ben@civ.zcu.cz>
2013-04-06 17:06:54 +0200

    create task routes, update express version to 3.1.1, add modules express-resource and express-namespace

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+//  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , resource = require('express-resource');
 
-var app = module.exports = express.createServer();
+var app = express();
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+		
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+		
 app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// app.get('/users', user.list);
+app.resource('task', require('./task'));
+		
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ routes/task.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET home page.
+ */
+
+module.exports = function(req, res){
+  res.render('task', { title: 'BAR Express' })
+};
