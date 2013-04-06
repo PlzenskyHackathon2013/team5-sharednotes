50374f63776888a665c0e7cb3e09dd302b1c3bda
Ondrej Sika <sika.ondrej@gmail.com>
2013-04-07 01:52:33 +0200

    add MONGOSKIN

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-var mongo = require("mongojs");
-
+var mongo = require("mongoskin");
+/*
 user = "admin";
 password = "pwvRIzrSLX5C";
 host = "127.0.0.1";
 port = 27018;
 database = "sharednotes";
-
-collections = ["users", "commons", "tasks"];
-url = "mongodb://"+user+":"+password+"@"+host+":"+port+"/"+database;
-exports.db = new mongo.connect(url, collections);
+*/
+//url = "mongodb://"+user+":"+password+"@"+host+":"+port+"/"+database;
+url = "mongodb://localhost/test";
+exports.db = mongo.db(url);
--- models.js
+++ models.js
@@ -1,7 +1,7 @@
 var db = require("./db").db;
 
 exports.createUser = function(username, email, password){
-    db.users.insert({
+    db.collection("users").insert({
          username: username,
          email: email,
          password:  password,
@@ -9,33 +9,31 @@ exports.createUser = function(username, email, password){
 }
 
 exports.userAuth = function(username, password, callback){
-    db.users.findOne({
+    db.collection("users").findOne({
         username: username,
         password: password,
-    },
-   function (err, user) {
+    }, function (err, user) {
         callback(user["_id"]);
     });
 }
 
 exports.getTaskByUser = function(userId, callback){
-    db.tasks.find({
+    db.collection("tasks").find({
         user_id: userId,
-    },
-   function (err, tasks) {
+    }).toArray(function (err, tasks) {
         callback(tasks);
     });
 }
 
 exports.isUserExists = function(username, callback) {
-    db.users.count({username: username},function (err, count){
+    db.collection("users").count({username: username},function (err, count){
         if(count) callback(true);
         else callback(false);    
     });
 }
 
 exports.createTask = function(user_id, title, description){
-    db.tasks.insert({
+    db.collection("tasks").insert({
          user_id: user_id,
          title: title,
          description: description,
@@ -43,7 +41,7 @@ exports.createTask = function(user_id, title, description){
 }
 
 exports.updateTask = function(task_id, title, description){
-    db.tasks.update({
+    db.collection("tasks").update({
         _id: task_id,
     },
     {
@@ -52,7 +50,7 @@ exports.updateTask = function(task_id, title, description){
     });
 }
 exports.getUser = function(user_id, callback) {
-    db.users.findOne({_id:user_id}, function(err, user){
+    db.collection("users").findOne({_id:user_id}, function(err, user){
         callback(user);
     });
 }
@@ -63,14 +61,4 @@ exports.getMailByUser = function(user_id, callback){
 
 exports.getNameByUser = function(user_id, callback){
     getUser(user_id,function (user) {callback(user["username"]);});
-}
-
-if (require.main === module){
-    userAuth("test", "test", function(id){
-    getTaskByUser(id, function(tasks){
-    console.log(tasks);
-    getMailByUser(id, console.log);
-    });
-    });
-    isUserExists("test", console.log);
 }
